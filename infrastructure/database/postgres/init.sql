b00b6dfa65e94b0f9a978806515a4008ce4fc03d
m-bugaj <bbugi2016@gmail.com>
2025-02-09 17:38:07 +0100

    Add database creation for microservice

--- infrastructure/database/postgres/init.sql
+++ infrastructure/database/postgres/init.sql
@@ -3,12 +3,18 @@ CREATE USER root WITH PASSWORD 'root';
 CREATE USER readonly_user WITH PASSWORD 'password';
 
 -- Tworzymy bazę danych, jeśli jeszcze nie istnieje
-CREATE DATABASE dev_beatsy;
+-- CREATE DATABASE dev_beatsy;
+CREATE DATABASE user_service;
 
 -- Przyznajemy pełne uprawnienia użytkownikowi root
 GRANT ALL PRIVILEGES ON DATABASE dev_beatsy TO root;
+GRANT ALL PRIVILEGES ON DATABASE user_service TO root;
 
 -- Przyznajemy tylko uprawnienia do odczytu użytkownikowi readonly_user
 GRANT CONNECT ON DATABASE dev_beatsy TO readonly_user;
+GRANT USAGE ON SCHEMA public TO readonly_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly_user;
+
+GRANT CONNECT ON DATABASE user_service TO readonly_user;
 GRANT USAGE ON SCHEMA public TO readonly_user;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly_user;
