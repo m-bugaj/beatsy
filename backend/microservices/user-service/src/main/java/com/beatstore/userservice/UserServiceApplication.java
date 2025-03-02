52771054958de12a7136db2e4ec197d567fe5ef1
m-bugaj <bbugi2016@gmail.com>
2025-03-02 16:39:40 +0100

    Fix Users table definition and improve database connection

--- backend/microservices/user-service/src/main/java/com/beatstore/userservice/UserServiceApplication.java
+++ backend/microservices/user-service/src/main/java/com/beatstore/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.beatstore.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+//@EntityScan(basePackages = "com.beatstore.userservice.model")
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
--- backend/microservices/user-service/src/main/resources/db_migrations/V1__0.0.0_0.0.1.sql
+++ backend/microservices/user-service/src/main/resources/db_migrations/V1__0.0.0_0.0.1.sql
@@ -1,8 +1,62 @@
-CREATE TABLE IF NOT EXISTS users (
-                                     id SERIAL PRIMARY KEY,
-                                     username VARCHAR(255) NOT NULL UNIQUE,
-                                     email VARCHAR(255) NOT NULL UNIQUE,
-                                     password_hash VARCHAR(255) NOT NULL,
-                                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                     modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- Tworzenie funkcji aktualizującej modified_at
+CREATE OR REPLACE FUNCTION set_modified_at()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    NEW.modified_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id            BIGINT PRIMARY KEY,
+    username      VARCHAR(255) NOT NULL UNIQUE,
+    email         VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    first_name    VARCHAR(255) NOT NULL,
+    last_name     VARCHAR(255) NOT NULL,
+    created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS roles
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(50) UNIQUE NOT NULL,
+    is_default  BOOLEAN            NOT NULL DEFAULT FALSE,
+    created_at  TIMESTAMP                   DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP                   DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS user_roles
+(
+    id          SERIAL PRIMARY KEY,
+    user_id     INT NOT NULL,
+    role_id     INT NOT NULL,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
+);
+
+
+-- Dodanie triggera do każdej tabeli
+CREATE TRIGGER trigger_users_modified_at
+    BEFORE UPDATE
+    ON users
+    FOR EACH ROW
+EXECUTE FUNCTION set_modified_at();
+
+CREATE TRIGGER trigger_roles_modified_at
+    BEFORE UPDATE
+    ON roles
+    FOR EACH ROW
+EXECUTE FUNCTION set_modified_at();
+
+CREATE TRIGGER trigger_user_roles_modified_at
+    BEFORE UPDATE
+    ON user_roles
+    FOR EACH ROW
+EXECUTE FUNCTION set_modified_at();
