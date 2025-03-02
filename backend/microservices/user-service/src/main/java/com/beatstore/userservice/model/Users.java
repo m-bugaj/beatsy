ff5f04ed27316dc6caff8e7833ff3417c10a07da
m-bugaj <bbugi2016@gmail.com>
2025-03-02 16:39:57 +0100

    Add User entity and map it to Users table

---
+++ backend/microservices/user-service/src/main/java/com/beatstore/userservice/model/Users.java
@@ -0,0 +1,43 @@
+package com.beatstore.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password_hash;
+
+    private String firstName;
+    private String lastName;
+
+//    @ManyToMany(fetch = FetchType.EAGER)
+//    @JoinTable(
+//            name = "user_roles",
+//            joinColumns = @JoinColumn(name = "user_id"),
+//            inverseJoinColumns = @JoinColumn(name = "role_id")
+//    )
+//    private Set<Role> roles = new HashSet<>();
+
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+}
