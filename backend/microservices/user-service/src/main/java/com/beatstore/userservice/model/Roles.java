bac920a3ecd399cfc41114ca959b0b79e5834e9a
m-bugaj <bbugi2016@gmail.com>
2025-03-03 08:31:20 +0100

    Add Roles entity

---
+++ backend/microservices/user-service/src/main/java/com/beatstore/userservice/model/Roles.java
@@ -0,0 +1,36 @@
+package com.beatstore.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Roles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Boolean isDefault;
+
+    //    @ManyToMany(fetch = FetchType.EAGER)
+//    @JoinTable(
+//            name = "user_roles",
+//            joinColumns = @JoinColumn(name = "user_id"),
+//            inverseJoinColumns = @JoinColumn(name = "role_id")
+//    )
+//    private Set<Role> roles = new HashSet<>();
+
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+}
