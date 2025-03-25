52c673aaefd041d7eaf7c9a36d60214de3e4182f
m-bugaj <bbugi2016@gmail.com>
2025-03-25 17:07:18 +0100

    Change id type to BIGSERIAL

--- backend/microservices/user-service/src/main/resources/db_migrations/V1__0.0.0_0.0.1.sql
+++ backend/microservices/user-service/src/main/resources/db_migrations/V1__0.0.0_0.0.1.sql
@@ -11,7 +11,7 @@ $$ LANGUAGE plpgsql;
 
 CREATE TABLE IF NOT EXISTS user_account
 (
-    id            BIGINT PRIMARY KEY,
+    id            BIGSERIAL PRIMARY KEY,
     username      VARCHAR(255) NOT NULL UNIQUE,
     email         VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
