0f63faba313d1203ca512d17e546b12c31e86ec0
m-bugaj <bbugi2016@gmail.com>
2025-04-15 07:25:43 +0200

    Add new database for marketplace microservice

--- infrastructure/database/postgres/init.sql
+++ infrastructure/database/postgres/init.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE user_service;
+CREATE DATABASE marketplace_service;
 
 -- Tworzymy użytkowników z prostymi hasłami
 CREATE USER root WITH PASSWORD 'root';
@@ -7,6 +8,7 @@ CREATE USER readonly_user WITH PASSWORD 'password';
 -- Przyznajemy pełne uprawnienia użytkownikowi root
 GRANT ALL PRIVILEGES ON DATABASE dev_beatsy TO root;
 GRANT ALL PRIVILEGES ON DATABASE user_service TO root;
+GRANT ALL PRIVILEGES ON DATABASE marketplace_service TO root;
 
 -- Przyznajemy tylko uprawnienia do odczytu użytkownikowi readonly_user
 GRANT CONNECT ON DATABASE dev_beatsy TO readonly_user;
@@ -14,5 +16,9 @@ GRANT USAGE ON SCHEMA public TO readonly_user;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly_user;
 
 GRANT CONNECT ON DATABASE user_service TO readonly_user;
+GRANT USAGE ON SCHEMA public TO readonly_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly_user;
+
+GRANT CONNECT ON DATABASE marketplace_service TO readonly_user;
 GRANT USAGE ON SCHEMA public TO readonly_user;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly_user;
