cc9a083b9bf6c52c56913990cfa0c4c9c41582ad
m-bugaj <bbugi2016@gmail.com>
2025-04-24 17:52:22 +0200

    SQL script refactor

--- backend/microservices/marketplace-service/src/main/resources/db_migrations/V1__0.0.0_0.0.1.sql
+++ backend/microservices/marketplace-service/src/main/resources/db_migrations/V1__0.0.0_0.0.1.sql
@@ -48,33 +48,33 @@ CREATE TABLE media_files
     CONSTRAINT fk_media_file_beat FOREIGN KEY (beat_id) REFERENCES beats (id) ON DELETE CASCADE
 );
 
-CREATE SEQUENCE licence_config_seq;
+CREATE SEQUENCE license_limit_config_id_seq;
 
-ALTER SEQUENCE licence_config_seq
+ALTER SEQUENCE license_limit_config_id_seq
     OWNER TO admin;
 
 CREATE TABLE license_limit_config
 (
-    id BIGINT DEFAULT NEXTVAL('licence_config_seq'::regclass) PRIMARY KEY,
+    id                      BIGINT    DEFAULT NEXTVAL('license_limit_config_id_seq'::regclass) PRIMARY KEY,
 
-    stream_limit INT,                                   -- np. 100000 (liczba streamów)
-    physical_sales_limit INT,                           -- np. 1000 (ile fizycznych kopii można sprzedać)
+    stream_limit            INT,                     -- np. 100000 (liczba streamów)
+    physical_sales_limit    INT,                     -- np. 1000 (ile fizycznych kopii można sprzedać)
 
-    allow_mp3_download BOOLEAN DEFAULT FALSE,           -- czy można pobierać MP3
-    allow_wav_download BOOLEAN DEFAULT FALSE,           -- czy można pobierać WAV
-    allow_trackout_download BOOLEAN DEFAULT FALSE,      -- czy można pobrać stems/trackout
+    allow_mp3_download      BOOLEAN   DEFAULT FALSE, -- czy można pobierać MP3
+    allow_wav_download      BOOLEAN   DEFAULT FALSE, -- czy można pobierać WAV
+    allow_stems_download    BOOLEAN   DEFAULT FALSE, -- czy można pobrać stems/trackout
 
-    allow_live_performance BOOLEAN DEFAULT FALSE,       -- czy można używać na płatnych koncertach
-    allow_broadcast BOOLEAN DEFAULT FALSE,              -- np. radio, tv
+    allow_live_performance  BOOLEAN   DEFAULT FALSE, -- czy można używać na płatnych koncertach
+    allow_broadcast         BOOLEAN   DEFAULT FALSE, -- np. radio, tv
 
-    monetization_allowed BOOLEAN DEFAULT FALSE,         -- np. na YouTube z adsense
-    use_in_paid_ads BOOLEAN DEFAULT FALSE,              -- czy może być używane w reklamach płatnych
-    use_in_video_projects BOOLEAN DEFAULT FALSE,        -- np. w filmach, YouTube itp.
+    allow_yt_monetization   BOOLEAN   DEFAULT FALSE, -- np. na YouTube z adsense
+    use_in_paid_ads         BOOLEAN   DEFAULT FALSE, -- czy może być używane w reklamach płatnych
+    use_in_video_projects   BOOLEAN   DEFAULT FALSE, -- np. w filmach, YouTube itp.
 
-    license_duration_months INT,                        -- np. 12 miesięcy licencji
+    license_duration_months INT,                     -- np. 12 miesięcy licencji
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at              TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at             TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE SEQUENCE licenses_id_seq;
@@ -84,13 +84,13 @@ ALTER SEQUENCE licenses_id_seq
 
 CREATE TABLE licenses
 (
-    id          BIGINT    DEFAULT NEXTVAL('licenses_id_seq'::regclass) PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    default_price NUMERIC(10,2) NOT NULL,
-    user_hash VARCHAR(255) NOT NULL,
-    license_limit_config_id BIGINT NOT NULL,
-    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id                      BIGINT    DEFAULT NEXTVAL('licenses_id_seq'::regclass) PRIMARY KEY,
+    name                    VARCHAR(255)   NOT NULL,
+    default_price           NUMERIC(10, 2) NOT NULL,
+    user_hash               VARCHAR(255)   NOT NULL,
+    license_limit_config_id BIGINT         NOT NULL,
+    created_at              TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at             TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_license_license_limit_config FOREIGN KEY (license_limit_config_id) REFERENCES license_limit_config (id)
 );
 
@@ -101,12 +101,12 @@ ALTER SEQUENCE beat_license_seq
 
 CREATE TABLE beat_license
 (
-    id          BIGINT    DEFAULT NEXTVAL('beat_license_seq'::regclass) PRIMARY KEY,
-    beat_id BIGINT NOT NULL,
-    license_id BIGINT NOT NULL,
-    custom_price NUMERIC(10,2),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id           BIGINT    DEFAULT NEXTVAL('beat_license_seq'::regclass) PRIMARY KEY,
+    beat_id      BIGINT NOT NULL,
+    license_id   BIGINT NOT NULL,
+    custom_price NUMERIC(10, 2),
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_beat_license_beat FOREIGN KEY (beat_id) REFERENCES beats (id),
     CONSTRAINT fk_beat_license_license FOREIGN KEY (license_id) REFERENCES licenses (id),
