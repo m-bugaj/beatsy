1bc774c4539f5b62834f104b69b59e16e4f7e9b5
m-bugaj <bbugi2016@gmail.com>
2025-09-08 07:20:51 +0200

    Implement security filter chain without csrf for dev environment

--- backend/microservices/api-gateway-web/src/main/java/com/beatstore/apigatewayweb/security/config/SecurityConfig.java
+++ backend/microservices/api-gateway-web/src/main/java/com/beatstore/apigatewayweb/security/config/SecurityConfig.java
@@ -1,20 +1,35 @@
 package com.beatstore.apigatewayweb.security.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 
+@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
+    @Profile("!dev")
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(csrf -> csrf
                         .csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
                 )
+                .authorizeExchange(exchange -> exchange
+                        .anyExchange().permitAll()
+                )
+                .build();
+    }
+    
+    @Bean
+    @Profile("dev")
+    public SecurityWebFilterChain securityFilterChainDev(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchange -> exchange
                         .anyExchange().permitAll()
                 )
