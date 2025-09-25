49c0d52181f0250d1d0b485bd0c3c1296580d9b2
m-bugaj <bbugi2016@gmail.com>
2025-09-25 15:57:15 +0200

    Rename filter

--- backend/microservices/marketplace-service/src/main/java/com/beatstore/marketplaceservice/context/filter/UserRequestContextFilter.java
+++ backend/microservices/marketplace-service/src/main/java/com/beatstore/marketplaceservice/context/filter/UserRequestContextFilter.java
@@ -6,22 +6,17 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Objects;
 
 @Component
-@Order(2)
-public class RequestContextFilter extends OncePerRequestFilter {
+public class UserRequestContextFilter extends OncePerRequestFilter {
 
     private final RequestContext requestContext;
 
-    public RequestContextFilter(RequestContext requestContext) {
+    public UserRequestContextFilter(RequestContext requestContext) {
         this.requestContext = requestContext;
     }
 
