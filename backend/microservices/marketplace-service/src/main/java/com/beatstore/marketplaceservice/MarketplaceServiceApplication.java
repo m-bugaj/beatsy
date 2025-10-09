3a0c9947dfdffe6c304a291fa6ad9f9ffd2b977d
m-bugaj <bbugi2016@gmail.com>
2025-10-09 07:07:39 +0200

    Enable feign in application

--- backend/microservices/marketplace-service/src/main/java/com/beatstore/marketplaceservice/MarketplaceServiceApplication.java
+++ backend/microservices/marketplace-service/src/main/java/com/beatstore/marketplaceservice/MarketplaceServiceApplication.java
@@ -2,8 +2,10 @@ package com.beatstore.marketplaceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MarketplaceServiceApplication {
 
 	public static void main(String[] args) {
