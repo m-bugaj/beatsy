4979b9aae29577ad8500bc91ed5c35cd7efd1450
m-bugaj <bbugi2016@gmail.com>
2025-10-10 19:48:48 +0200

    Initialize user-service

---
+++ backend/microservices/user-service/src/main/java/com/beatstore/userservice/UserServiceeApplication.java
@@ -0,0 +1,13 @@
+package com.beatstore.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceeApplication.class, args);
+	}
+
+}
---
+++ backend/microservices/user-service/src/test/java/com/beatstore/userservicee/UserServiceeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beatstore.userservicee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
