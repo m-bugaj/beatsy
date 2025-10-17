5ee4c73acc1c92ba16982491079667210941de91
m-bugaj <bbugi2016@gmail.com>
2025-10-17 20:12:04 +0200

    Init and link auth_db after microservice rename

--- infrastructure/database/postgres/init.sql
+++ infrastructure/database/postgres/init.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE auth_db;
 CREATE DATABASE user_service;
 CREATE DATABASE marketplace_service;
 CREATE DATABASE api_gateway;
@@ -8,12 +9,17 @@ CREATE USER readonly_user WITH PASSWORD 'password';
 
 -- Przyznajemy pełne uprawnienia użytkownikowi root
 GRANT ALL PRIVILEGES ON DATABASE dev_beatsy TO root;
+GRANT ALL PRIVILEGES ON DATABASE auth_db TO root;
 GRANT ALL PRIVILEGES ON DATABASE user_service TO root;
 GRANT ALL PRIVILEGES ON DATABASE marketplace_service TO root;
 GRANT ALL PRIVILEGES ON DATABASE api_gateway TO root;
 
 -- Przyznajemy tylko uprawnienia do odczytu użytkownikowi readonly_user
 GRANT CONNECT ON DATABASE dev_beatsy TO readonly_user;
+GRANT USAGE ON SCHEMA public TO readonly_user;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly_user;
+
+GRANT CONNECT ON DATABASE auth_db TO readonly_user;
 GRANT USAGE ON SCHEMA public TO readonly_user;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO readonly_user;
 
