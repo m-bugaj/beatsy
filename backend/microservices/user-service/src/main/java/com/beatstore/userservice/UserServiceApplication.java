710301f35f03d6c1dae87f4c2c2945cf7ba03027
m-bugaj <bbugi2016@gmail.com>
2025-10-21 17:51:47 +0200

    Refactor: fix typo in class name

--- backend/microservices/user-service/src/main/java/com/beatstore/userservice/UserServiceApplication.java
+++ backend/microservices/user-service/src/main/java/com/beatstore/userservice/UserServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceeApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceeApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
