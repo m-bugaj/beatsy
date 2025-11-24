fcf00a322192aec4233db888e9d3b253c5455d5a
m-bugaj <bbugi2016@gmail.com>
2025-11-24 07:56:38 +0100

    Migration script init

---
+++ backend/microservices/content-service/src/main/resources/db_migrations/V1__0.0.0_0.0.1.sql
@@ -0,0 +1,50 @@
+-- Tworzenie funkcji aktualizującej modified_at
+CREATE OR REPLACE FUNCTION set_modified_at()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    NEW.modified_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE SEQUENCE content_id_seq;
+
+ALTER SEQUENCE content_id_seq
+    OWNER TO admin;
+
+CREATE TABLE content
+(
+    id          BIGINT    DEFAULT NEXTVAL('content_id_seq'::regclass) NOT NULL PRIMARY KEY,
+    hash        VARCHAR(255) UNIQUE                                 NOT NULL,
+    user_hash   VARCHAR(255)                                        NOT NULL,
+    type    VARCHAR(255) NOT NULL,
+    title       VARCHAR(255) UNIQUE                                 NOT NULL,
+    description VARCHAR(255),
+    visibility  VARCHAR(255)                                        NOT NULL,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CREATE TABLE beat_details
+-- (
+--     id          BIGINT    DEFAULT NEXTVAL('beats_id_seq'::regclass) NOT NULL PRIMARY KEY,
+--     hash        VARCHAR(255) UNIQUE                                 NOT NULL,
+--     user_hash   VARCHAR(255)                                        NOT NULL,
+--     title       VARCHAR(255) UNIQUE                                 NOT NULL,
+--     description VARCHAR(255),
+--     bpm         INT,
+--     mood        VARCHAR(255)                                        NOT NULL,
+--     visibility  VARCHAR(255)                                        NOT NULL,
+--     created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+
+
+-- Dodanie triggera do każdej tabeli
+CREATE TRIGGER trigger_content_modified_at
+    BEFORE UPDATE
+    ON content
+    FOR EACH ROW
+EXECUTE FUNCTION set_modified_at();
