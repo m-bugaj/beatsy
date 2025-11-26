36d9b874276a611ebcb7fc868b8175c6df36aa9e
m-bugaj <bbugi2016@gmail.com>
2025-11-26 07:26:56 +0100

    Map Content entity to db model

---
+++ backend/microservices/content-service/src/main/java/com/beatstore/contentservice/model/Content.java
@@ -0,0 +1,51 @@
+package com.beatstore.contentservice.model;
+
+import com.beatstore.contentservice.common.enums.ContentType;
+import com.beatstore.contentservice.common.enums.ContentVisibility;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "content")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Content {
+    @Id
+    @SequenceGenerator(
+            name = "content_id_seq",
+            sequenceName = "content_id_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "content_id_seq"
+    )
+    private Long id;
+
+    @Column(nullable = false)
+    private String hash;
+
+    @Column(nullable = false)
+    private String userHash;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ContentType type;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ContentVisibility visibility;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+}
