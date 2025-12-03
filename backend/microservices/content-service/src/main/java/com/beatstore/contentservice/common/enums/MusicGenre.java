586d8a3443d7a76f1b95c02fdc6b050bca0be6c1
m-bugaj <bbugi2016@gmail.com>
2025-12-03 19:24:38 +0100

    Init enum with basic music genres

---
+++ backend/microservices/content-service/src/main/java/com/beatstore/contentservice/common/enums/MusicGenre.java
@@ -0,0 +1,14 @@
+package com.beatstore.contentservice.common.enums;
+
+public enum MusicGenre {
+    HIP_HOP,
+    TRAP,
+    LOFI,
+    RNB,
+    POP,
+    EDM,
+    HOUSE,
+    DRILL,
+    REGGAETON,
+    DANCEHALL
+}
