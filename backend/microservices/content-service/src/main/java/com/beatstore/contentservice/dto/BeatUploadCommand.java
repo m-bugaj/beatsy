26fc076479a01e93e2a1a53042c60a51e4555118
m-bugaj <bbugi2016@gmail.com>
2025-12-08 07:43:15 +0100

    Add beat dtos

---
+++ backend/microservices/content-service/src/main/java/com/beatstore/contentservice/dto/BeatUploadCommand.java
@@ -0,0 +1,29 @@
+package com.beatstore.contentservice.dto;
+
+
+import com.beatstore.marketplaceservice.common.enums.ContentVisibility;
+import com.beatstore.marketplaceservice.common.enums.beat.BeatGenre;
+import com.beatstore.marketplaceservice.common.enums.beat.BeatMood;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Getter
+public class BeatUploadCommand {
+    @JsonIgnore
+    @Setter
+    private String userHash;
+
+    private String title;
+    private String description;
+    private Integer bpm;
+    private Set<BeatGenre> genres;
+    private BeatMood mood;
+    private ContentVisibility visibility;
+
+    private Set<String> licenseHashes;
+    private BigDecimal customPrice;
+}
