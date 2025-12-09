6be2715bf296ab1f3172dbcdc8c37851d2be6991
m-bugaj <bbugi2016@gmail.com>
2025-12-09 18:19:13 +0100

    Add security config and required filters to app

---
+++ backend/microservices/content-service/src/main/java/com/beatstore/contentservice/context/filter/UserRequestContextFilter.java
@@ -0,0 +1,36 @@
+package com.beatstore.contentservice.context.filter;
+
+import com.beatstore.contentservice.context.RequestContext;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class UserRequestContextFilter extends OncePerRequestFilter {
+
+    private final RequestContext requestContext;
+
+    public UserRequestContextFilter(RequestContext requestContext) {
+        this.requestContext = requestContext;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String userHash = request.getHeader("X-User-Hash");
+
+        requestContext.setUserHash(userHash);
+
+        filterChain.doFilter(request, response);
+    }
+}
