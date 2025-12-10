425aff67f6a8270870cda087633f68d2fdbdc618
m-bugaj <bbugi2016@gmail.com>
2025-12-10 08:00:58 +0100

    Add enum with hard-coded beat moods

---
+++ backend/microservices/content-service/src/main/java/com/beatstore/contentservice/common/enums/beat/BeatMood.java
@@ -0,0 +1,14 @@
+package com.beatstore.contentservice.common.enums.beat;
+
+public enum BeatMood {
+    HAPPY,
+    SAD,
+    DARK,
+    CHILL,
+    ROMANTIC,
+    ENERGETIC,
+    AGGRESSIVE,
+    UPLIFTING,
+    MELLOW,
+    INSPIRATIONAL
+}
