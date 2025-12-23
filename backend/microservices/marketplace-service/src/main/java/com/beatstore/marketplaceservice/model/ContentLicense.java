bc66c608d4ad73f77e25278f24a165e016f25d4b
m-bugaj <bbugi2016@gmail.com>
2025-12-23 07:44:59 +0100

    Map ContentLicense entity to database table

---
+++ backend/microservices/marketplace-service/src/main/java/com/beatstore/marketplaceservice/model/ContentLicense.java
@@ -0,0 +1,44 @@
+package com.beatstore.marketplaceservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "content_license")
+@Getter
+@Setter
+@NoArgsConstructor
+public class ContentLicense {
+    @Id
+    @SequenceGenerator(
+            name = "content_license_id_seq",
+            sequenceName = "content_license_id_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "content_license_id_seq"
+    )
+    private Long id;
+
+    private String contentHash;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "license_id",
+            referencedColumnName = "id",
+            nullable = false
+    )
+    private License license;
+
+    private BigDecimal customPrice;
+    private boolean active;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+}
