a39f6a4a52a5a0197ba0d5ff6b5fd104febd5ba8
m-bugaj <bbugi2016@gmail.com>
2026-03-02 08:29:09 +0100

    Initial service setup for buyer license management

---
+++ backend/microservices/license-service/src/main/java/com/beatstore/licenseservice/LicenseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.beatstore.licenseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LicenseServiceApplication.class, args);
+    }
+
+}
